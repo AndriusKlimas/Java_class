dbd662e64cd9c44b299306365e188af54f2cc95a
AndriusKlimas <andrius.klimas01@gmail.com>
2026-09-14 11:59:12 +0100

    2

--- trial/src/main/java/in_class/DemoUserIO.java
+++ trial/src/main/java/in_class/DemoUserIO.java
@@ -5,13 +5,42 @@ import java.util.Scanner;
 public class DemoUserIO {
     static void main(){
 
+
+        //String
         System.out.println("Please enter name");
         Scanner input = new Scanner(System.in);
         String name = input.nextLine();
 
         System.out.println("Hello how are you " + name);
 
-        //trial22
+        //Comparing String
+        if(name.equals("Fred")){
+            System.out.println("You are banned");
+        }else {
+            System.out.println("Welcome in");
+        }
+
+        // ! means not
+        if(!name.equals("Fred")){
+            System.out.println("Welcome");
+        }else {
+            System.out.println("Get out");
+        }
+
+
+
+        // numbers
+
+        System.out.println("Please enter your age");
+        int age = input.nextInt();
+
+        if(age <18 ) {
+            System.out.println("You are too young, get out");
+        } else if (age<25) {
+            System.out.println("No car renteal for you");
+        }else{
+            System.out.println("Come in");
+    }
     }
 
 }
---
+++ trial/src/main/java/in_class/SecurityQuestion.java
@@ -0,0 +1,10 @@
+package in_class;
+
+public class SecurityQuestion {
+    static void main(){
+        String accepteable = "bing";
+
+        System.out.println();
+
+    }
+}
