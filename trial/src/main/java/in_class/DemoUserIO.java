9ec4418380fff85e314e8b581dfc4331186dc390
andri <andrius.klimas01@gmail.com>
2026-09-14 11:21:40 +0100

    finished simple input output

---
+++ trial/src/main/java/in_class/DemoUserIO.java
@@ -0,0 +1,15 @@
+package in_class;
+
+import java.util.Scanner;
+
+public class DemoUserIO {
+    static void main(){
+
+        System.out.println("Please enter name");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
+        System.out.println("Hello how are you " + name);
+    }
+
+}
