260808f85a253bc4fbf230db7fec8236c958df91
AndriusKlimas <andrius.klimas01@gmail.com>
2026-09-14 12:01:36 +0100

    Creating a verification code

--- trial/src/main/java/in_class/SecurityQuestion.java
+++ trial/src/main/java/in_class/SecurityQuestion.java
@@ -1,10 +1,16 @@
 package in_class;
 
+import java.util.Scanner;
+
 public class SecurityQuestion {
     static void main(){
         String accepteable = "bing";
 
         System.out.println();
+        Scanner input = new Scanner(System.in);
+        String answear = input.nextLine();
+
+        if()
 
     }
 }
